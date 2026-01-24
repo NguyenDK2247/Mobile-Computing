ec0d11fd4bfe551fdb8cdaf3fcf8953d4083ba39
NguyenDK2247 <127587120+NguyenDK2247@users.noreply.github.com>
2026-01-24 04:02:07 +0200

    modified application now with 2 pages and updated README.md

--- README.md
+++ README.md
@@ -1,1 +1,107 @@
-### This is a test for HW2
+# HW2 description
+
+### 1. To implement navigation into the application, first it's important to get the right imports:
+``` kotlin
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+```
+
+- Then define a `navController` using `rememberNavController()`, the `modifier` to be used for later:
+``` kotlin
+val modifier = Modifier
+val navController = rememberNavController()
+```
+
+- Create a `NavHost` block that sets the address of the navigation screens.
+Here we set the `startDestination` page as `main`, and navigating to `secondary`:
+``` kotlin
+NavHost(modifier = modifier, navController = navController, startDestination = "main") {
+    composable("main") { MainScreen(navController) }
+    composable("secondary") { SecondaryScreen(navController) }
+}
+```
+
+- Once that is set up, now we have two functions, corresponding with two different screens:
+``` kotlin
+@Composable
+fun MainScreen(navController: NavHostController) {
+    var buttonClicked by remember { mutableStateOf(false) }
+    Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            LazyColumn(
+            ) {
+                items(SampleData.conversationSample) {
+                    message -> MessageCard(message)
+                }
+
+                item {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(vertical = 16.dp),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Column(
+                            verticalArrangement = Arrangement.Center,
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            modifier = Modifier.fillMaxHeight()
+                        ) {
+                            Text(
+                                text = "Click this button below for a surprise :3",
+                                style = MaterialTheme.typography.bodyMedium,
+                                textAlign = TextAlign.Center,
+                                modifier = Modifier.fillMaxWidth()
+                            )
+                            Spacer(modifier = Modifier.height(8.dp))
+                            Button(
+                                onClick = {
+                                    buttonClicked = !buttonClicked
+                                    navController.navigate("secondary")
+                                },
+                            ) {
+                                Text("Go!")
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+```
+
+- There are a few important implementations here:
+  - `fun MainScreen(navController: NavHostController)`: set the previously defined
+`navController` in relation to `navHostController`.
+  - `navController.navigate("secondary")`, this is for the button that will direct the user to the other page(s).
+In this case, it is `secondary`.
+
+- We do similar implementation for the other function, but there is another required addition, which we will get back to later.
+- For the button that navigates pages:
+  - First define a boolean variable `var buttonClicked by remember { mutableStateOf(false) }`.
+  - Then set it inside `Button` where `buttonClicked` is altered between `true` and `false`:
+    ``` kotlin
+    Button(
+        onClick = {
+            buttonClicked = !buttonClicked
+            navController.navigate("secondary")
+        },
+    ) {
+        Text("Go!")
+    }
+    ```
+    
+### 2. Circular navigation prevention
+- Only one line is needed:
+``` kotlin
+navController.popBackStack()
+```
+This occurs in the `Button` block of the **non-main** function.
--- app/src/main/java/com/example/mobilecomputing/MainActivity.kt
+++ app/src/main/java/com/example/mobilecomputing/MainActivity.kt
@@ -2,38 +2,106 @@
 
 package com.example.mobilecomputing
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.navigation.NavController
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.mobilecomputing.ui.theme.MobileComputingTheme
-import android.util.Log
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MobileComputingTheme {
-                // Create a NavHost with NavController
+                val modifier = Modifier
                 val navController = rememberNavController()
-                NavHost(navController = navController, startDestination = "main") {
-                    composable("main") { MainScreen(onNavigateToComplex = { navController.navigate("complex") }) }
-                    composable("complex") { ComplexScreen(onNavigateBack = {
-                        navController.popBackStack()
-                    }) }
+                NavHost(modifier = modifier, navController = navController, startDestination = "main") {
+                    composable("main") { MainScreen(navController) }
+                    composable("secondary") { SecondaryScreen(navController) }
+                }
+            }
+        }
+    }
+}
+
+data class Message(val author: String, val body: String)
+
+@Composable
+fun MainScreen(navController: NavHostController) {
+    var buttonClicked by remember { mutableStateOf(false) }
+    Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            LazyColumn(
+            ) {
+                items(SampleData.conversationSample) {
+                    message -> MessageCard(message)
+                }
+
+                item {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(vertical = 16.dp),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Column(
+                            verticalArrangement = Arrangement.Center,
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            modifier = Modifier.fillMaxHeight()
+                        ) {
+                            Text(
+                                text = "Click this button below for a surprise :3",
+                                style = MaterialTheme.typography.bodyMedium,
+                                textAlign = TextAlign.Center,
+                                modifier = Modifier.fillMaxWidth()
+                            )
+                            Spacer(modifier = Modifier.height(8.dp))
+                            Button(
+                                onClick = {
+                                    buttonClicked = !buttonClicked
+                                    navController.navigate("secondary")
+                                },
+                            ) {
+                                Text("Go!")
+                            }
+                        }
+                    }
                 }
             }
         }
@@ -41,7 +109,7 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MainScreen(onNavigateToComplex: () -> Unit) {
+fun SecondaryScreen(navController: NavHostController) {
     Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
         Column(
             modifier = Modifier
@@ -50,44 +118,108 @@ fun MainScreen(onNavigateToComplex: () -> Unit) {
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text("Main Screen")
+            Text(
+                text = "Congratulations on making it this far! There's nothing here yet though :p",
+                style = MaterialTheme.typography.bodyMedium,
+                textAlign = TextAlign.Center,
+            )
             Spacer(modifier = Modifier.height(16.dp))
-            Button(onClick = { onNavigateToComplex() }) {
-                Text("Go to Complex Screen")
+            Button(
+                onClick = {
+                    navController.popBackStack()
+                },
+            ) {
+                Text("Back")
             }
         }
     }
 }
 
 @Composable
-fun ComplexScreen(onNavigateBack: () -> Unit) {
-    Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-        Column(
+fun MessageCard(msg: Message) {
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+        Image(
+            painter = painterResource(R.drawable.profile_picture),
+            contentDescription = null,
             modifier = Modifier
-                .fillMaxSize()
-                .padding(16.dp),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Text("Complex Screen")
-            Spacer(modifier = Modifier.height(16.dp))
-            Button(onClick = { onNavigateBack() }) {
-                Text("Back to Main Screen")
+                .size(40.dp)
+                .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colorScheme.primary, CircleShape)
+        )
+
+        Spacer(modifier = Modifier.width(8.dp))
+
+        var isExpanded by remember { mutableStateOf(false) }
+        val surfaceColor by animateColorAsState(
+            if (isExpanded) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.surface,
+        )
+
+        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
+            Text(
+                text = msg.author,
+                color = MaterialTheme.colorScheme.secondary,
+                style = MaterialTheme.typography.titleSmall
+            )
+
+            Spacer(modifier = Modifier.height(4.dp))
+
+            Surface(
+                shape = MaterialTheme.shapes.medium,
+                shadowElevation = 1.dp,
+                color = surfaceColor,
+                modifier = Modifier
+                    .animateContentSize()
+                    .padding(1.dp)
+            ) {
+                Text(
+                    text = msg.body,
+                    modifier = Modifier.padding(all = 4.dp),
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
+                    style = MaterialTheme.typography.bodyMedium
+                )
             }
         }
     }
 }
+
+@Preview(name = "Light Mode")
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode"
+)
+@Composable
+fun PreviewMessageCard() {
+    MobileComputingTheme {
+        Surface {
+            MessageCard(
+                msg = Message("Lexi", "Take a look at Jetpack Compose, it's great!")
+            )
+        }
+    }
+}
+
+//@Composable
+//fun Conversation(messages: List<Message>) {
+//    LazyColumn {
+//        items(messages) { message ->
+//            MessageCard(message)
+//        }
+//    }
+//}
 
 @Preview(showBackground = true)
 @Composable
 fun PreviewMainScreen() {
-    MobileComputingTheme { MainScreen(onNavigateToComplex = {}) }
+    val navController = rememberNavController()
+    MainScreen(navController)
 }
 
 @Preview(showBackground = true)
 @Composable
-fun PreviewComplexScreen() {
-    MobileComputingTheme { ComplexScreen(onNavigateBack = {}) }
+fun PreviewSecondaryScreen() {
+    val navController = rememberNavController()
+    SecondaryScreen(navController)
 }
 
 
